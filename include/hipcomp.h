fb0ca5e6f274e76342d20c5cc28ffa8003f2f6a9
Dominic Etienne Charrier <docharri@amd.com>
2023-08-24 18:40:28 +0200

    refactor(inlcude/hipcomp.h): merge w. 2.2

--- include/hipcomp.h
+++ include/hipcomp.h
@@ -31,6 +31,7 @@
 #define HIPCOMP_H
 
 #include <hip/hip_runtime.h>
+#include "hipcomp/shared_types.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -41,9 +42,11 @@ extern "C" {
  *****************************************************************************/
 
 #define HIPCOMP_MAJOR_VERSION 2
-#define HIPCOMP_MINOR_VERSION 1
+#define HIPCOMP_MINOR_VERSION 2
 #define HIPCOMP_PATCH_VERSION 0
 
+//: DELETED
+#if 0
 typedef enum hipcompStatus_t
 {
   hipcompSuccess = 0,
@@ -53,6 +56,8 @@ typedef enum hipcompStatus_t
   hipcompErrorHipError = 1000,
   hipcompErrorInternal = 10000,
 } hipcompStatus_t;
+#endif
+//: END DELETED
 
 /* Supported datatypes */
 typedef enum hipcompType_t
