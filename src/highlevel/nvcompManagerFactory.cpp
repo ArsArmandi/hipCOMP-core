e8bfe2407a20bb32e5dee6f03bd9bf634d01402b
Dominic Etienne Charrier <docharri@amd.com>
2023-08-25 16:51:41 +0200

    refactor(src/highlevel/nvcompManagerFactory.cpp): hipify

--- src/highlevel/nvcompManagerFactory.cpp
+++ src/highlevel/nvcompManagerFactory.cpp
@@ -25,38 +25,39 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+// Modifications Copyright (C) 2023 Advanced Micro Devices, Inc. All rights reserved.
 
 #include <assert.h>
 
-#include "nvcomp.hpp"
-#include "nvcomp/nvcompManager.hpp"
-#include "nvcomp/ans.hpp"
-#include "nvcomp/lz4.hpp"
-#include "nvcomp/snappy.hpp"
-#include "nvcomp/gdeflate.hpp"
-#include "nvcomp/cascaded.hpp"
-#include "nvcomp/bitcomp.hpp"
-#include "nvcomp_common_deps/hlif_shared_types.hpp"
-#include "CudaUtils.h"
+#include "hipcomp.hpp"
+#include "hipcomp/hipcompManager.hpp"
+#include "hipcomp/ans.hpp"
+#include "hipcomp/lz4.hpp"
+#include "hipcomp/snappy.hpp"
+#include "hipcomp/gdeflate.hpp"
+#include "hipcomp/cascaded.hpp"
+#include "hipcomp/bitcomp.hpp"
+#include "hipcomp_common_deps/hlif_shared_types.hpp"
+#include "HipUtils.h"
 
-namespace nvcomp {
+namespace hipcomp {
 
-std::shared_ptr<nvcompManagerBase> create_manager(const uint8_t* comp_buffer, cudaStream_t stream = 0, const int device_id = 0) {
+std::shared_ptr<hipcompManagerBase> create_manager(const uint8_t* comp_buffer, hipStream_t stream = 0, const int device_id = 0) {
   // Need to determine the type of manager
   const CommonHeader* common_header = reinterpret_cast<const CommonHeader*>(comp_buffer);
   CommonHeader cpu_common_header;
-  CudaUtils::check(cudaMemcpyAsync(&cpu_common_header, common_header, sizeof(CommonHeader), cudaMemcpyDefault, stream));
-  CudaUtils::check(cudaStreamSynchronize(stream));
+  HipUtils::check(hipMemcpyAsync(&cpu_common_header, common_header, sizeof(CommonHeader), hipMemcpyDefault, stream));
+  HipUtils::check(hipStreamSynchronize(stream));
 
-  std::shared_ptr<nvcompManagerBase> res;
+  std::shared_ptr<hipcompManagerBase> res;
 
   switch(cpu_common_header.format) {
     case FormatType::LZ4: 
     {
       LZ4FormatSpecHeader format_spec;
       const LZ4FormatSpecHeader* gpu_format_header = reinterpret_cast<const LZ4FormatSpecHeader*>(comp_buffer + sizeof(CommonHeader));
-      CudaUtils::check(cudaMemcpyAsync(&format_spec, gpu_format_header, sizeof(LZ4FormatSpecHeader), cudaMemcpyDefault, stream));
-      CudaUtils::check(cudaStreamSynchronize(stream));
+      HipUtils::check(hipMemcpyAsync(&format_spec, gpu_format_header, sizeof(LZ4FormatSpecHeader), hipMemcpyDefault, stream));
+      HipUtils::check(hipStreamSynchronize(stream));
 
       res = std::make_shared<LZ4Manager>(cpu_common_header.uncomp_chunk_size, format_spec.data_type, stream, device_id);
       break;
@@ -65,18 +66,18 @@ std::shared_ptr<nvcompManagerBase> create_manager(const uint8_t* comp_buffer, cu
     {
       SnappyFormatSpecHeader format_spec;
       const SnappyFormatSpecHeader* gpu_format_header = reinterpret_cast<const SnappyFormatSpecHeader*>(comp_buffer + sizeof(CommonHeader));
-      CudaUtils::check(cudaMemcpyAsync(&format_spec, gpu_format_header, sizeof(SnappyFormatSpecHeader), cudaMemcpyDefault, stream));
-      CudaUtils::check(cudaStreamSynchronize(stream));
+      HipUtils::check(hipMemcpyAsync(&format_spec, gpu_format_header, sizeof(SnappyFormatSpecHeader), hipMemcpyDefault, stream));
+      HipUtils::check(hipStreamSynchronize(stream));
       
       res = std::make_shared<SnappyManager>(cpu_common_header.uncomp_chunk_size, stream, device_id);
       break;
     }
     case FormatType::GDeflate: 
     {
-      nvcompBatchedGdeflateOpts_t format_spec;
-      const nvcompBatchedGdeflateOpts_t* gpu_format_header = reinterpret_cast<const nvcompBatchedGdeflateOpts_t*>(comp_buffer + sizeof(CommonHeader));
-      CudaUtils::check(cudaMemcpyAsync(&format_spec, gpu_format_header, sizeof(nvcompBatchedGdeflateOpts_t), cudaMemcpyDefault, stream));
-      CudaUtils::check(cudaStreamSynchronize(stream));
+      hipcompBatchedGdeflateOpts_t format_spec;
+      const hipcompBatchedGdeflateOpts_t* gpu_format_header = reinterpret_cast<const hipcompBatchedGdeflateOpts_t*>(comp_buffer + sizeof(CommonHeader));
+      HipUtils::check(hipMemcpyAsync(&format_spec, gpu_format_header, sizeof(hipcompBatchedGdeflateOpts_t), hipMemcpyDefault, stream));
+      HipUtils::check(hipStreamSynchronize(stream));
 
       res = std::make_shared<GdeflateManager>(cpu_common_header.uncomp_chunk_size, format_spec.algo, stream, device_id);
       break;
@@ -86,12 +87,12 @@ std::shared_ptr<nvcompManagerBase> create_manager(const uint8_t* comp_buffer, cu
 #ifdef ENABLE_BITCOMP
       BitcompFormatSpecHeader format_spec;
       const BitcompFormatSpecHeader* gpu_format_header = reinterpret_cast<const BitcompFormatSpecHeader*>(comp_buffer + sizeof(CommonHeader));
-      CudaUtils::check(cudaMemcpyAsync(&format_spec, gpu_format_header, sizeof(BitcompFormatSpecHeader), cudaMemcpyDefault, stream));
-      CudaUtils::check(cudaStreamSynchronize(stream));
+      HipUtils::check(hipMemcpyAsync(&format_spec, gpu_format_header, sizeof(BitcompFormatSpecHeader), hipMemcpyDefault, stream));
+      HipUtils::check(hipStreamSynchronize(stream));
 
       res = std::make_shared<BitcompManager>(format_spec.data_type, format_spec.algo, stream, device_id);
 #else
-      throw NVCompException(nvcompErrorNotSupported, "Bitcomp support not available in this build.");
+      throw HIPCompException(hipcompErrorNotSupported, "Bitcomp support not available in this build.");
 #endif
       break;
     }
@@ -99,8 +100,8 @@ std::shared_ptr<nvcompManagerBase> create_manager(const uint8_t* comp_buffer, cu
     {
       ANSFormatSpecHeader format_spec;
       const ANSFormatSpecHeader* gpu_format_header = reinterpret_cast<const ANSFormatSpecHeader*>(comp_buffer + sizeof(CommonHeader));
-      CudaUtils::check(cudaMemcpyAsync(&format_spec, gpu_format_header, sizeof(ANSFormatSpecHeader), cudaMemcpyDefault, stream));
-      CudaUtils::check(cudaStreamSynchronize(stream));
+      HipUtils::check(hipMemcpyAsync(&format_spec, gpu_format_header, sizeof(ANSFormatSpecHeader), hipMemcpyDefault, stream));
+      HipUtils::check(hipStreamSynchronize(stream));
 
       res = std::make_shared<ANSManager>(cpu_common_header.uncomp_chunk_size, stream, device_id);
       break;
@@ -109,8 +110,8 @@ std::shared_ptr<nvcompManagerBase> create_manager(const uint8_t* comp_buffer, cu
     {
       CascadedFormatSpecHeader format_spec;
       const CascadedFormatSpecHeader* gpu_format_header = reinterpret_cast<const CascadedFormatSpecHeader*>(comp_buffer + sizeof(CommonHeader));
-      CudaUtils::check(cudaMemcpyAsync(&format_spec, gpu_format_header, sizeof(CascadedFormatSpecHeader), cudaMemcpyDefault, stream));
-      CudaUtils::check(cudaStreamSynchronize(stream));
+      HipUtils::check(hipMemcpyAsync(&format_spec, gpu_format_header, sizeof(CascadedFormatSpecHeader), hipMemcpyDefault, stream));
+      HipUtils::check(hipStreamSynchronize(stream));
 
       assert(cpu_common_header.uncomp_chunk_size == format_spec.options.chunk_size);
 
@@ -126,5 +127,5 @@ std::shared_ptr<nvcompManagerBase> create_manager(const uint8_t* comp_buffer, cu
   return res;
 }
 
-} // namespace nvcomp 
+} // namespace hipcomp 
  
