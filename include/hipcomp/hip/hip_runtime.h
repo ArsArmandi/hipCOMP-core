dce391f54b6635483e630463cf23237592a0d40b
domcharrier <docharri@amd.com>
2023-08-29 09:33:09 -0700

    refactor: hip/nvidia build exports hipcomp/hip/*.h files now

--- include/hipcomp/hip/hip_runtime.h
+++ include/hipcomp/hip/hip_runtime.h
@@ -1,5 +1,31 @@
-#include "cuda_runtime.h"
+// MIT License
+// 
+// Copyright (c) 2023 Advanced Micro Devices, Inc.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef HIP_INCLUDE_HIP_HIP_RUNTIME_H
+#define HIP_INCLUDE_HIP_HIP_RUNTIME_H
+
 #if defined(__HIP_PLATFORM_NVCC__) || defined(__HIP_PLATFORM_NVIDIA__)
+  #include "cuda_runtime.h"
+  
   #define hipDevAttrComputeCapabilityMajor cudaDevAttrComputeCapabilityMajor
   #define hipDeviceGetAttribute cudaDeviceGetAttribute
   #define hipDeviceProp_t cudaDeviceProp
@@ -40,4 +66,8 @@
   #define hipStreamSynchronize cudaStreamSynchronize
   #define hipStream_t cudaStream_t
   #define hipSuccess cudaSuccess
+#else
+  #error only use for HIP/NVIDIA compile path!
 #endif
+
+#endif // HIP_INCLUDE_HIP_HIP_RUNTIME_H
--- include_ext_nvidia/hip/hip_cooperative_groups.h
+++
@@ -1,1 +0,0 @@
-#include <cooperative_groups.h>
